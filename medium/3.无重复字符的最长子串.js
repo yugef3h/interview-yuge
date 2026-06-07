d242b423413250bb70a026acd90518f114ab7793
yugef3h <ql1993xl@gmail.com>
2026-06-07 08:52:17 +0800

    feat: update

--- medium/3.无重复字符的最长子串.js
+++ medium/3.无重复字符的最长子串.js
@@ -6,6 +6,7 @@
 
 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+     1143是最长公共子序列，3是最长无重复字符子串
  */
 
 
@@ -23,3 +24,5 @@ var lengthOfLongestSubstring = function(s) {
   }
   return len
 }
+
+
